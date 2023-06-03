987958e563f4e3ed4799a98bf8416d83a9bff8a9
mharv <mitch.k.harvey@gmail.com>
2023-06-03 18:29:55 +0100

    fixed up css formatting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -72,9 +72,11 @@ function App() {
                     </p>
 
                     <button className="bg-midnight px-6 py-2 text-white rounded-full" onClick={getRandomStringSet} >choose 6 strings at random</button>
+                    <div className="grid grid-cols-6 gap-4">
                     {stringSet && stringSet.map((guitarString) => {
-                        return <p key={guitarString}>{guitarStringNumberLookup[guitarString] + ": " + guitarString}</p>
+                        return <p className="text-3xl font-bold "key={guitarString}>{guitarStringNumberLookup[guitarString] + ": " + guitarString}</p>
                     })}
+                    </div>
 
                     <br />
                     <br />
