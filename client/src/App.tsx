b07025a216a567d4610fb9d01b336ab279308ff1
Mitchell Harvey <mharv@pop-os.localdomain>
2023-06-03 17:58:35 +0100

    added tailwind to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './App.css';
 
 const guitarStringNumberLookup: { [key: string]: number } = {
     'E': 6,
@@ -28,19 +27,67 @@ function App() {
     }
 
     const [stringSet, setStringSet] = useState<string[]>();
+    const [noteSet, setNoteSet] = useState<string[]>();
+
+    const generateRandomNote = () => {
+        //including sharps
+        const notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
+        const randomNote = notes[Math.floor(Math.random() * notes.length)];
+        return randomNote;
+    }
+
+    const convertSharpNoteToFlat = (note: string) => {
+        const sharpToFlatLookup: { [key: string]: string } = {
+            'A#': 'Bb',
+            'C#': 'Db',
+            'D#': 'Eb',
+            'F#': 'Gb',
+            'G#': 'Ab'
+        };
+        return sharpToFlatLookup[note];
+    }
+
+    const generateRandomNoteSet = () => {
+        const noteSet = new Set();
+        while (noteSet.size < 12) {
+            let randomNote = generateRandomNote();
+            if (randomNote.includes('#')) {
+                if (Math.random() < 0.5) {
+                    randomNote = convertSharpNoteToFlat(randomNote);
+                }
+            }
+            noteSet.add(randomNote);
+        }
+        // convert the set to an array
+        const noteSetArray = Array.from(noteSet);
+        setNoteSet(noteSetArray as string[]);
+    }
 
     return (
         <div className="App">
-            <header className="App-header">
-                <p>
-                    GPP - Guitar practice platform
+            <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+                <p className="text-3xl text-gray-700 font-bold mb-5">
+                    pracGPP - Guitar practice platform
                 </p>
 
-                <button onClick={getRandomStringSet} >choose 6 strings at random</button>
+                <button className="bg-midnight px-6 py-2 text-white rounded-full" onClick={getRandomStringSet} >choose 6 strings at random</button>
                 {stringSet && stringSet.map((guitarString) => {
                     return <p key={guitarString}>{guitarStringNumberLookup[guitarString] + ": " + guitarString}</p>
                 })}
-            </header>
+
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <button className="bg-metal px-6 py-2 text-white rounded-full" onClick={generateRandomNoteSet} >choose 12 notes at random</button>
+                <p className="text-3xl text-gray-700 font-bold mb-5">Notes:</p>
+                <p>
+                    {noteSet && noteSet.map((note) => {
+                        return <span key={note}>{"  " + note + "  "}</span>
+                    })}
+                </p>
+            </div>
+
         </div>
     );
 }
