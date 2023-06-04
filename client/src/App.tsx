095e0c586a08d0d4986bcaadd0d51ce94034a3bd
mharv <mitch.k.harvey@gmail.com>
2023-06-04 07:45:36 +0100

    added toggle for string note display

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -61,41 +61,50 @@ function App() {
         setNoteSet(noteSetArray as string[]);
     }
 
+    const [showStringNumberOnly, setShowStringNumberOnly] = useState<boolean>(false);
+
     useEffect(() => {
         getRandomStringSet();
         generateRandomNoteSet();
     }, []);
-    
+
     return (
         <div className="App">
             <div className="container mx-auto p-8 m-10">
                 <p className="text-3xl text-gray-700 font-bold mb-5">
                     PracGPP - Guitar practice platform
                 </p>
-            <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-                <div className="flex items-center">
-                    <p className="text-3xl text-gray-700 font-bold mb-5">String order:</p>
-                    <button className="bg-midnight px-6 py-2 ml-5 mb-5 text-white rounded-full" onClick={getRandomStringSet} >choose 6 strings at random</button>
-                </div>
-                <div className="grid grid-cols-6 gap-4">
-                    {stringSet && stringSet.map((guitarString) => {
-                        return <p className="text-3xl font-bold " key={guitarString}>{guitarStringNumberLookup[guitarString] + ": " + guitarString}</p>
-                    })}
-                </div>
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <div className="flex items-center">
-                    <p className="text-3xl text-gray-700 font-bold mb-5">Notes:</p>
-                    <button className="bg-metal px-6 py-2 ml-5 mb-5 text-white rounded-full" onClick={generateRandomNoteSet} >choose 12 notes at random</button>
-                </div>
-                <div className="grid grid-cols-12 gap-4">
-                    {noteSet && noteSet.map((note) => {
-                        return <p className="text-3xl font-bold"><span key={note}>{"  " + note + "  "}</span></p>
-                    })}
-                </div>
+                <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+                    <div className="flex items-center">
+                        <p className="text-3xl text-gray-700 font-bold mb-5">String order:</p>
+                        <button className="bg-midnight px-6 py-2 ml-5 mb-5 text-white rounded-full" onClick={getRandomStringSet} >choose 6 strings at random</button>
+                        <button className="bg-midnight px-6 py-2 ml-5 mb-5 text-white rounded-full" onClick={() => setShowStringNumberOnly(!showStringNumberOnly)} >toggle string notes on/off</button>
+                    </div>
+                    {showStringNumberOnly ?
+                        <div className="grid grid-cols-6 gap-4">
+                            {stringSet && stringSet.map((guitarString) => {
+                                return <p className="text-5xl font-bold " key={guitarString}>{guitarStringNumberLookup[guitarString]}</p>
+                            })}
+                        </div> :
+                        <div className="grid grid-cols-6 gap-4">
+                            {stringSet && stringSet.map((guitarString) => {
+                                return <p className="text-5xl font-bold " key={guitarString}>{guitarStringNumberLookup[guitarString] + ": " + guitarString}</p>
+                            })}
+                        </div>}
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <div className="flex items-center">
+                        <p className="text-3xl text-gray-700 font-bold mb-5">Notes:</p>
+                        <button className="bg-metal px-6 py-2 ml-5 mb-5 text-white rounded-full" onClick={generateRandomNoteSet} >choose 12 notes at random</button>
+                    </div>
+                    <div className="grid grid-cols-12 gap-4">
+                        {noteSet && noteSet.map((note) => {
+                            return <p className="text-5xl font-bold"><span key={note}>{"  " + note + "  "}</span></p>
+                        })}
+                    </div>
                 </div>
             </div>
         </div>
